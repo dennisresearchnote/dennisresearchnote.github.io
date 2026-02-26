6e1d05a4c477c285fe6a08413eef861b725992dc
dennisInfoSecCookBook <andy970326@gmail.com>
2026-02-26 21:26:58 +0900

    Add GitHub Actions deploy for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://dennisinfoseccookbook.github.io',
-    // 유저 페이지(루트)라면 base 설정을 두지 않습니다.
 });
