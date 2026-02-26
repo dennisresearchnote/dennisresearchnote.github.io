588b842d5fddf76379cbd7d7eb39991e62421636
dennisInfoSecCookBook <andy970326@gmail.com>
2026-02-26 20:47:38 +0900

    Deploy site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dennisinfoseccookbook.github.io',
-    base: '/dennisinfoseccookbook',
+    // 유저 페이지(루트)라면 base 설정을 두지 않습니다.
 });
