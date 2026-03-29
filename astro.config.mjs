7f164795d0945b31860662337201d2320dfcc284
dennisresearchnote <91735325+dennisresearchnote@users.noreply.github.com>
2026-03-29 09:00:45 +0000

    modified

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import remarkChirpyAttrs from './src/plugins/remark-chirpy-attrs.mjs';
 import rehypeCodeTitles from './src/plugins/rehype-code-titles.mjs';
-import rehypeMermaid from './src/plugins/rehype-mermaid.mjs';
 
 export default defineConfig({
   site: 'https://dennisresearchnote.github.io/',
